e4c12180fade1da14414f831ff08d7d0ef660aeb
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2012-03-29 21:59:18 +0200

    cmake build for retro apps

---
+++ retro68.toolchain.cmake
@@ -0,0 +1,11 @@
+set( CMAKE_SYSTEM_NAME Retro68 )
+set( CMAKE_SYSTEM_VERSION 1)
+
+set( RETRO68_ROOT "" CACHE PATH "path to root of Retro68 Toolchain" )
+
+set( MAKE_APPL "${RETRO68_ROOT}/bin/MakeAPPL" )
+
+set( CMAKE_C_COMPILER "${RETRO68_ROOT}/bin/m68k-unknown-elf-gcc" )
+set( CMAKE_CXX_COMPILER "${RETRO68_ROOT}/bin/m68k-unknown-elf-g++" )
+
+set( CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,-elf2flt -Wl,-q -l:retrocrt.o")
