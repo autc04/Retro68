2194af326d8e145ded4b2bb712c609253df3d4d0
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2012-04-06 00:45:04 +0200

    cmake build for libretro

--- retro68.toolchain.cmake
+++ retro68.toolchain.cmake
@@ -2,10 +2,12 @@ set( CMAKE_SYSTEM_NAME Retro68 )
 set( CMAKE_SYSTEM_VERSION 1)
 
 set( RETRO68_ROOT "" CACHE PATH "path to root of Retro68 Toolchain" )
+set( CMAKE_INSTALL_PREFIX "${RETRO68_ROOT}/m68k-unknown-elf/" CACHE PATH "installation prefix" )
 
 set( MAKE_APPL "${RETRO68_ROOT}/bin/MakeAPPL" )
 
 set( CMAKE_C_COMPILER "${RETRO68_ROOT}/bin/m68k-unknown-elf-gcc" )
 set( CMAKE_CXX_COMPILER "${RETRO68_ROOT}/bin/m68k-unknown-elf-g++" )
 
-set( CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,-elf2flt -Wl,-q -l:retrocrt.o")
+set( CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,-elf2flt -Wl,-q" )
+
