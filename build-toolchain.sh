6b82186aad2aa51428971e2c53eeb67c3a992866
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2012-04-06 20:56:16 +0200

    Mac OS X (host) compatibility, build cmake things from build scripts

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -1,11 +1,11 @@
-SRC=$(readlink -f `dirname $0`)
+SRC=$(cd `dirname $0` && pwd -P)
 mkdir -p binutils-build
 mkdir -p toolchain
 PREFIX=`pwd`/toolchain/
 set -e
 
 cd binutils-build
-export "CFLAGS=-Wno-unused-but-set-variable -Wno-unused-but-set-parameter"
+export "CFLAGS=-Wno-error"
 $SRC/binutils/configure --target=m68k-unknown-elf --prefix=$PREFIX
 make -j8
 make install
@@ -20,20 +20,20 @@ make install
 
 cd ..
 
-#mkdir -p newlib-build
-#cd newlib-build
-BINUTILS=$(readlink -f binutils-build)
+BINUTILS=$(cd binutils-build && pwd -P)
 
-export "CFLAGS=-I../../Retro68/binutils/include"
+cp $SRC/elf.h $PREFIX/include/
+export "CFLAGS=-I../../Retro68/binutils/include -I../toolchain/include"
 mkdir -p elf2flt-build
 cd elf2flt-build
 $SRC/elf2flt/configure --target=m68k-unknown-elf --prefix=$PREFIX --with-binutils-build-dir=$BINUTILS
-make -j8
+make -j8 TOOLDIR=$PREFIX/bin
 make install
 unset CFLAGS
 
 cd ..
 
+mkdir -p $PREFIX/man/man1
 rm -rf hfsutils
 cp -r $SRC/hfsutils .
 cd hfsutils
@@ -44,3 +44,18 @@ cd ..
 
 runhaskell ../Retro68/PrepareHeaders.hs ../Retro68/Universal\ Headers toolchain/m68k-unknown-elf/include
 
+mkdir -p build-host
+cd build-host
+cmake ../../Retro68/ -DCMAKE_INSTALL_PREFIX=$PREFIX
+cd ..
+
+mkdir -p build-target
+cd build-target
+cmake ../../Retro68/ -DCMAKE_INSTALL_PREFIX=$PREFIX/m68k-unknown-elf \
+					-DCMAKE_TOOLCHAIN_FILE=$SRC/retro68.toolchain.cmake \
+					-DRETRO68_ROOT=$PREFIX \
+					-DCMAKE_BUILD_TYPE=Release
+cd ..
+
+make -C build-host install
+make -C build-target install
