82203b6b7d5ced43da180820e2ca545a962d5f35
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2012-04-24 03:22:36 +0200

    add README, COPYING and license notices

--- retro68.toolchain.cmake
+++ retro68.toolchain.cmake
@@ -1,3 +1,20 @@
+#   Copyright 2012 Wolfgang Thaller.
+#
+#   This file is part of Retro68.
+#
+#   Retro68 is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Retro68 is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Retro68.  If not, see <http://www.gnu.org/licenses/>.
+
 set( CMAKE_SYSTEM_NAME Retro68 )
 set( CMAKE_SYSTEM_VERSION 1)
 
