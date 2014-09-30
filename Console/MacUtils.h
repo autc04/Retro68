04d8465b3bab77ab867560ad2e422e6112bc4791
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2014-09-30 19:20:52 +0200

    remove some dead code

--- Console/MacUtils.h
+++ Console/MacUtils.h
@@ -36,11 +36,3 @@ public:
    }
 };
 
-inline const unsigned char* operator"" _pstr(const char*p, size_t n)
-{
-	unsigned char *pp = reinterpret_cast<unsigned char*>(const_cast<char*>(p));
-	pp[0] = n-1;
-	return pp;
-}
-#define PSTR(s) ("*" s ## _pstr)
-
