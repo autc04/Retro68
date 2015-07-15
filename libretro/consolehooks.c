0561012bb63e386b76f4a5699cd857ee7259ea06
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2015-07-15 23:23:19 +0200

    Add GCC Runtime Library Exception to license for libretro

--- libretro/consolehooks.c
+++ libretro/consolehooks.c
@@ -1,3 +1,28 @@
+/*
+	Copyright 2015 Wolfgang Thaller.
+
+	This file is part of Retro68.
+
+	Retro68 is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Retro68 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	Under Section 7 of GPL version 3, you are granted additional
+	permissions described in the GCC Runtime Library Exception, version
+	3.1, as published by the Free Software Foundation.
+
+	You should have received a copy of the GNU General Public License and
+	a copy of the GCC Runtime Library Exception along with this program;
+	see the files COPYING and COPYING.RUNTIME respectively.  If not, see
+	<http://www.gnu.org/licenses/>.
+*/
+
 #include <sys/types.h>
 
 __attribute__((weak)) ssize_t _consolewrite(int fd, const void *buf, size_t count)
--- libretro/glue.c
+++ libretro/glue.c
@@ -1,5 +1,5 @@
 /*
-	Copyright 2012 Wolfgang Thaller.
+	Copyright 2015 Wolfgang Thaller.
 
 	This file is part of Retro68.
 
@@ -13,8 +13,14 @@
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with Retro68.  If not, see <http://www.gnu.org/licenses/>.
+	Under Section 7 of GPL version 3, you are granted additional
+	permissions described in the GCC Runtime Library Exception, version
+	3.1, as published by the Free Software Foundation.
+
+	You should have received a copy of the GNU General Public License and
+	a copy of the GCC Runtime Library Exception along with this program;
+	see the files COPYING and COPYING.RUNTIME respectively.  If not, see
+	<http://www.gnu.org/licenses/>.
 */
 
 #include <MacMemory.h>
