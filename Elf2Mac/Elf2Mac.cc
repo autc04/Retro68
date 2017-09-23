008ffe1401c7d09bdb16a8942d4e356e7cd78c73
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2017-09-23 03:56:37 +0200

    Elf2Mac, a replacement for elf2flt. Right now this is not an improvement, but the plan is to work towards multi-segment apps.

---
+++ Elf2Mac/Elf2Mac.cc
@@ -0,0 +1,295 @@
+/*
+	 Copyright 2017 Wolfgang Thaller.
+
+	 This file is part of Retro68.
+
+	 Retro68 is free software: you can redistribute it and/or modify
+	 it under the terms of the GNU General Public License as published by
+	 the Free Software Foundation, either version 3 of the License, or
+	 (at your option) any later version.
+
+	 Retro68 is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 GNU General Public License for more details.
+
+	 You should have received a copy of the GNU General Public License
+	 along with Retro68.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Elf2Mac.h"
+
+#include "ResourceFork.h"
+#include "BinaryIO.h"
+#include "ResourceFile.h"
+
+#include <gelf.h>
+
+#include <err.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <fstream>
+#include <iostream>
+
+#include <boost/algorithm/string/predicate.hpp>
+
+using std::string;
+using std::unordered_map;
+using std::vector;
+using std::ofstream;
+
+unordered_map<string, Elf_Scn*> codeSections;
+unordered_map<string, Elf_Scn*> relaSections;
+Elf_Scn* dataSection;
+Elf_Scn* bssSection;
+Elf_Scn *symtabSection;
+
+void ElfToFlt(string input, string output)
+{
+	std::cout << "************ ELF2FLT\n";
+	if(elf_version ( EV_CURRENT ) == EV_NONE)
+		errx(EXIT_FAILURE , "ELF library initialization failed: %s", elf_errmsg( -1));
+
+	int fd = open(input.c_str(), O_RDONLY, 0);
+	Elf *e = elf_begin(fd, ELF_C_READ, NULL);
+
+	size_t shstrndx;
+	elf_getshdrstrndx(e, &shstrndx);
+
+	GElf_Ehdr ehdr;
+	gelf_getehdr(e, &ehdr);
+
+	Elf_Scn *symtab = NULL;
+	int nSyms;
+
+	for(Elf_Scn *scn = NULL; (scn = elf_nextscn(e, scn)) != NULL;)
+	{
+		GElf_Shdr shdr;
+		gelf_getshdr(scn, &shdr);
+		if(shdr.sh_type == SHT_SYMTAB)
+		{
+			symtab = scn;
+			nSyms = shdr.sh_size / shdr.sh_entsize;
+			break;
+		}
+	}
+	Elf_Data *symtabData = elf_getdata(symtab, NULL);
+
+	std::vector<int> relocs;
+
+	for(Elf_Scn *scn = NULL; (scn = elf_nextscn(e, scn)) != NULL;)
+	{
+		GElf_Shdr shdr;
+		gelf_getshdr(scn, &shdr);
+		std::string name = elf_strptr(e, shstrndx, shdr.sh_name);
+		printf("section: %s\n", name.c_str());
+
+		if(shdr.sh_type == SHT_RELA
+		        && !bssSection)	// ignore everything after bss, that's just debug info
+		{
+			printf("(rela)\n");
+
+			int nRela = shdr.sh_size / shdr.sh_entsize;
+			Elf_Data *data = elf_getdata(scn, NULL);
+			for(int i = 0; i < nRela; i++)
+			{
+				GElf_Rela rela;
+				gelf_getrela(data, i, &rela);
+
+				if(GELF_R_TYPE(rela.r_info) == R_68K_32)
+					relocs.push_back(rela.r_offset);
+				//printf("rel: %d %d %x %x\n", (int)GELF_R_TYPE(rela.r_info), (int)GELF_R_SYM(rela.r_info), (unsigned)rela.r_addend, (unsigned)rela.r_offset);
+			}
+
+			if(boost::algorithm::starts_with(name,".rela."))
+			{
+				relaSections[name.substr(5)] = scn;
+			}
+		}
+		if(shdr.sh_type == SHT_PROGBITS
+		        && !bssSection)	// ignore everything after bss, that's just debug info
+		{
+			codeSections[name] = scn;
+			printf("(progbits)\n");
+		}
+		if(shdr.sh_type == SHT_NOBITS)
+		{
+			bssSection = scn;
+			printf("(nobits)\n");
+		}
+	}
+
+	/*for(int i = 0; i < nSyms; i++)
+	{
+		GElf_Sym sym;
+		if(gelf_getsym(symtabData, i, &sym) == 0)
+			printf("computer says no.\n");
+		printf("%s at %x\n", elf_strptr(e, shstrndx-1, sym.st_name), (unsigned) sym.st_value);
+	}*/
+
+	std::sort(relocs.begin(), relocs.end());
+
+	ofstream out(output);
+
+	byte(out, 'b');
+	byte(out, 'F');
+	byte(out, 'L');
+	byte(out, 'T');
+	longword(out, 4);
+	longword(out, 0x40 + ehdr.e_entry);	// entry point
+
+	GElf_Shdr text_shdr, data_shdr, bss_shdr;
+	gelf_getshdr(codeSections[".text"], &text_shdr);
+	gelf_getshdr(codeSections[".data"], &data_shdr);
+	gelf_getshdr(bssSection, &bss_shdr);
+
+	longword(out, 0x40 + data_shdr.sh_addr); // data start
+	longword(out, 0x40 + bss_shdr.sh_addr);	// data end
+	longword(out, 0x40 + bss_shdr.sh_addr + bss_shdr.sh_size);	// bss end
+	longword(out, 4096); // stack size, ignored
+	longword(out, 0x40 + bss_shdr.sh_addr); // relocStart);
+	longword(out, relocs.size());
+	longword(out, 0); // flags
+	for(int i = 0; i < 6; i++)
+		longword(out, 0); // filler
+
+	Elf_Data *data = elf_getdata(codeSections[".text"], NULL);
+	out << string((char*)data->d_buf, (char*)data->d_buf + data->d_size);
+
+	while(out.tellp() < 0x40 + data_shdr.sh_addr)
+		byte(out, 0x00);
+	data = elf_getdata(codeSections[".data"], NULL);
+	out << string((char*)data->d_buf, (char*)data->d_buf + data->d_size);
+
+	while(out.tellp() < 0x40 + bss_shdr.sh_addr)
+		byte(out, 0x00);
+	for(int reloc : relocs)
+		longword(out, reloc);
+}
+
+string argvZero;
+
+void RealLD(vector<string> args)
+{
+	vector<const char*> argv;
+	string realLD = argvZero + ".real";
+	argv.push_back(realLD.c_str());
+	for(string& s : args)
+		argv.push_back(s.c_str());
+	argv.push_back(NULL);
+
+	pid_t pid = fork();
+	if(pid < 0)
+	{
+		perror("unable to fork");
+		exit(-1);
+	}
+	else if(pid == 0)
+	{
+		execvp(argv[0], const_cast<char* const *> (argv.data()));
+		perror("exec failed");
+		exit(-1);
+	}
+	else
+	{
+		int wstatus;
+		int result = 0;
+		do
+		{
+			result = waitpid(pid, &wstatus, 0);
+		} while(result == -1 && errno == EINTR);
+		if(!WIFEXITED(wstatus))
+		{
+			errx(EXIT_FAILURE, "ld process did not exit properly");
+		}
+		else
+		{
+			int exitcode = WEXITSTATUS(wstatus);
+			if(exitcode)
+				exit(exitcode);
+		}
+	}
+
+}
+
+int main(int argc, char *argv[])
+{
+	vector<string> args;
+	std::copy(argv + 1, argv+argc, std::back_inserter(args));
+	argvZero = argv[0];
+
+	if(boost::algorithm::ends_with(argv[0], "ld"))
+	{
+		string outputFile = "a.out";
+		bool elf2flt = false;
+		bool elf2mac = false;
+
+		vector<string> args2;
+		for(auto p = args.begin(), e = args.end(); p != e; ++p)
+		{
+			if(*p == "-o")
+			{
+				++p;
+				if(p == e)
+					errx(EXIT_FAILURE, "-o missing argument");
+				outputFile = *p;
+			}
+			else if(boost::algorithm::starts_with(*p, "-o"))
+			{
+				outputFile = (*p).substr(2);
+			}
+			else if(*p == "-elf2flt")
+			{
+				elf2flt = true;
+			}
+			else if(*p == "-elf2mac")
+			{
+				elf2mac = true;
+			}
+			else
+			{
+				args2.push_back(*p);
+			}
+		}
+
+		if(elf2mac)
+		{
+			errx(EXIT_FAILURE, "-elf2mac: not yet implemented");
+		}
+		else if(elf2flt)
+		{
+			char tmpfile[] = "/tmp/ldscriptXXXXXX";
+			int fd = mkstemp(tmpfile);
+			if(fd < 0)
+				errx(EXIT_FAILURE, "can't create temp file");
+			{
+				ofstream out(tmpfile);
+				CreateLdScript(out);
+			}
+			args2.push_back("-o");
+			args2.push_back(outputFile + ".gdb");
+			args2.push_back("-T");
+			args2.push_back(tmpfile);
+			RealLD(args2);
+			unlink(tmpfile);
+			ElfToFlt(outputFile + ".gdb", outputFile);
+		}
+		else
+		{
+			RealLD(args);
+		}
+		return 0;
+	}
+	else
+	{
+		if(argc != 2)
+			errx(EXIT_FAILURE, "usage : %s file-name ", argv[0]);
+		ElfToFlt(argv[1], "out.flt");
+	}
+	return 0;
+}
---
+++ Elf2Mac/LdScript.cc
@@ -0,0 +1,156 @@
+/*
+	 Copyright 2017 Wolfgang Thaller.
+
+	 This file is part of Retro68.
+
+	 Retro68 is free software: you can redistribute it and/or modify
+	 it under the terms of the GNU General Public License as published by
+	 the Free Software Foundation, either version 3 of the License, or
+	 (at your option) any later version.
+
+	 Retro68 is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 GNU General Public License for more details.
+
+	 You should have received a copy of the GNU General Public License
+	 along with Retro68.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Elf2Mac.h"
+
+#include <fstream>
+
+const char * defaultLdScript = R"ld(/* ld script for Elf2Mac */
+ENTRY( _start )
+SECTIONS
+{
+   .text :	{
+        */libretrocrt.a:*(.text*)
+       */foo.o(.text*)
+       *(.text*)
+       _stext = . ;
+
+       *(.stub)
+       *(.gnu.linkonce.t*)
+       *(.glue_7t)
+       *(.glue_7)
+       *(.jcr)
+       . = ALIGN (4) ;
+       __init_section = . ;
+       KEEP (*(.init))
+       __init_section_end = . ;
+       __fini_section = . ;
+       KEEP (*(.fini))
+       __fini_section_end = . ;
+       _etext = . ;
+
+       *(.eh_frame_hdr)
+       KEEP(*(.eh_frame))
+       KEEP(*(.gcc_except_table))
+       KEEP(*(.gcc_except_table.*))
+   }
+   .data : {
+       . = ALIGN(0x4) ;
+       _sdata = . ;
+               *(.got.plt)
+               *(.got)
+               FILL(0) ;
+               . = ALIGN(0x20) ;
+               LONG(-1)
+       . = ALIGN(0x20) ;
+       *(.rodata)
+       *(.rodata1)
+       *(.rodata.*)
+       *(.gnu.linkonce.r*)
+       *(.data)
+       *(.data1)
+       *(.data.*)
+       *(.gnu.linkonce.d*)
+
+               . = ALIGN(4) ;
+       /* gcc uses crtbegin.o to find the start of
+          the constructors, so we make sure it is
+          first.  Because this is a wildcard, it
+          doesn't matter if the user does not
+          actually link against crtbegin.o; the
+          linker won't look for a file to match a
+          wildcard.  The wildcard also means that it
+          doesn't matter which directory crtbegin.o
+          is in.  */
+       KEEP (*crtbegin*.o(.ctors))
+       /* We don't want to include the .ctor section from
+          from the crtend.o file until after the sorted ctors.
+          The .ctor section from the crtend file contains the
+          end of ctors marker and it must be last */
+       KEEP (*(EXCLUDE_FILE (*crtend*.o ) .ctors))
+       KEEP (*(SORT(.ctors.*)))
+       KEEP (*(.ctors))
+       KEEP (*crtbegin*.o(.dtors))
+       KEEP (*(EXCLUDE_FILE (*crtend*.o ) .dtors))
+       KEEP (*(SORT(.dtors.*)))
+       KEEP (*(.dtors))
+
+       *(.tm_clone_table)
+   }
+   .bss : {
+               _sbss = ALIGN(0x4) ;
+               __bss_start = . ;
+               *(.dynsbss)
+               *(.sbss)
+               *(.sbss.*)
+               *(.scommon)
+               *(.dynbss)
+               *(.bss)
+               *(.bss.*)
+               *(.bss*)
+               *(.gnu.linkonce.b*)
+               *(COMMON)
+               . = ALIGN(0x10) ;
+               _ebss = . ;
+   }
+       /DISCARD/ : { *(.note.GNU-stack) }
+       /* Stabs debugging sections.    */
+       .stab 0 : { *(.stab) }
+       .stabstr 0 : { *(.stabstr) }
+       .stab.excl 0 : { *(.stab.excl) }
+       .stab.exclstr 0 : { *(.stab.exclstr) }
+       .stab.index 0 : { *(.stab.index) }
+       .stab.indexstr 0 : { *(.stab.indexstr) }
+       .comment 0 : { *(.comment) }
+       /* DWARF debug sections.
+          Symbols in the DWARF debugging sections are relative to the beginning
+          of the section so we begin them at 0.  */
+       /* DWARF 1 */
+       .debug          0 : { *(.debug) }
+       .line           0 : { *(.line) }
+       /* GNU DWARF 1 extensions */
+       .debug_srcinfo  0 : { *(.debug_srcinfo) }
+       .debug_sfnames  0 : { *(.debug_sfnames) }
+       /* DWARF 1.1 and DWARF 2 */
+       .debug_aranges  0 : { *(.debug_aranges) }
+       .debug_pubnames 0 : { *(.debug_pubnames) }
+       /* DWARF 2 */
+       .debug_info     0 : { *(.debug_info .gnu.linkonce.wi.*) }
+       .debug_abbrev   0 : { *(.debug_abbrev) }
+       .debug_line     0 : { *(.debug_line) }
+       .debug_frame    0 : { *(.debug_frame) }
+       .debug_str      0 : { *(.debug_str) }
+       .debug_loc      0 : { *(.debug_loc) }
+       .debug_macinfo  0 : { *(.debug_macinfo) }
+       /* SGI/MIPS DWARF 2 extensions */
+       .debug_weaknames 0 : { *(.debug_weaknames) }
+       .debug_funcnames 0 : { *(.debug_funcnames) }
+       .debug_typenames 0 : { *(.debug_typenames) }
+       .debug_varnames  0 : { *(.debug_varnames) }
+
+   /* /DISCARD/ : { *(*) } */
+}
+
+)ld";
+
+
+void CreateLdScript(std::ofstream& out)
+{
+	out << defaultLdScript;
+}
