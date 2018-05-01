b86da1d4067784eea3c9065426d855e9119ce0e3
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2018-05-01 19:37:13 +0200

    Save all registers for LoadSeg
    This prevents crashes when calling a struct-returning function triggers a LoadSeg
    (the address for the returned struct is passed via A0)

--- libretro/LoadSeg.s
+++ libretro/LoadSeg.s
@@ -7,15 +7,29 @@ PATCHEDLOADSEG:
 #   (arguments for function)
 #   (return address)
 #   (return address from trap (== address in jt entry))
-    movel %sp@, %sp@-
+    movem.l  %a0-%a1/%d0-%d2, -(%sp)
+    subq #4, %sp
+    move.l 24(%sp), -(%sp)
     
 # Stack now:
 #   (arguments for function)
 #   (return address)
-#   (don't care)
-#   (return address from trap (== address in jt entry))
+#   (don't care any more -- address in jt entry)
+#   (saved registers)
+#   (4 bvtes of space for return value)
+#   (address in jt entry)
 
     jsr RETRO68LOADSEGMENT
+
+# Stack now:
+#   (arguments for function)
+#   (return address)
+#   (don't care -- address in jt entry)
+#   (saved registers)
+#   (address of loaded function)
+
+    move.l (%sp)+, 20(%sp)
+    movem.l (%sp)+, %a0-%a1/%d0-%d2
 
 # Stack now:
 #   (arguments for function)
