97a082224446556ac6337294e0770cdbcf5e4afe
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2023-12-26 23:14:38 +0100

    add platform-specific samples packages

--- flake.nix
+++ flake.nix
@@ -48,35 +48,39 @@
                 } // cross)
             self'.legacyPackages.crossPkgs;
 
-          packages.tools = pkgs.retro68.tools;
-          packages.hfsutils = pkgs.retro68.hfsutils;
+          packages = {
+            tools = pkgs.retro68.tools;
+            hfsutils = pkgs.retro68.hfsutils;
 
-          packages.default = pkgs.runCommand "Retro68" {} ''
-            mkdir $out
-            mkdir $out/m68k-apple-macos
-            mkdir $out/powerpc-apple-macos
-            ${pkgs.xorg.lndir}/bin/lndir -silent ${self'.legacyPackages.crossPkgs.m68k.retro68.libretro}/. $out/m68k-apple-macos
-            ${pkgs.xorg.lndir}/bin/lndir -silent ${self'.legacyPackages.crossPkgs.m68k.retro68.multiversal}/. $out/m68k-apple-macos
-            ${pkgs.xorg.lndir}/bin/lndir -silent ${self'.legacyPackages.crossPkgs.powerpc.retro68.libretro}/. $out/powerpc-apple-macos
-            ${pkgs.xorg.lndir}/bin/lndir -silent ${self'.legacyPackages.crossPkgs.powerpc.retro68.multiversal}/. $out/powerpc-apple-macos
-            ${pkgs.xorg.lndir}/bin/lndir -silent ${self'.legacyPackages.crossPkgs.carbon.retro68.libretro}/. $out/powerpc-apple-macos
-            ${pkgs.xorg.lndir}/bin/lndir -silent ${self'.legacyPackages.crossPkgs.carbon.retro68.multiversal}/. $out/powerpc-apple-macos
-            ${pkgs.xorg.lndir}/bin/lndir -silent ${pkgs.retro68.tools}/. $out
-            ${pkgs.xorg.lndir}/bin/lndir -silent ${pkgs.retro68.hfsutils}/. $out
-            ${pkgs.xorg.lndir}/bin/lndir -silent ${pkgs.cmake}/. $out
-            ${pkgs.xorg.lndir}/bin/lndir -silent ${pkgs.gnumake}/. $out
-            ${pkgs.xorg.lndir}/bin/lndir -silent ${pkgs.ninja}/. $out
+            default = pkgs.runCommand "Retro68" {} ''
+              mkdir $out
+              mkdir $out/m68k-apple-macos
+              mkdir $out/powerpc-apple-macos
+              ${pkgs.xorg.lndir}/bin/lndir -silent ${self'.legacyPackages.crossPkgs.m68k.retro68.libretro}/. $out/m68k-apple-macos
+              ${pkgs.xorg.lndir}/bin/lndir -silent ${self'.legacyPackages.crossPkgs.m68k.retro68.multiversal}/. $out/m68k-apple-macos
+              ${pkgs.xorg.lndir}/bin/lndir -silent ${self'.legacyPackages.crossPkgs.powerpc.retro68.libretro}/. $out/powerpc-apple-macos
+              ${pkgs.xorg.lndir}/bin/lndir -silent ${self'.legacyPackages.crossPkgs.powerpc.retro68.multiversal}/. $out/powerpc-apple-macos
+              ${pkgs.xorg.lndir}/bin/lndir -silent ${self'.legacyPackages.crossPkgs.carbon.retro68.libretro}/. $out/powerpc-apple-macos
+              ${pkgs.xorg.lndir}/bin/lndir -silent ${self'.legacyPackages.crossPkgs.carbon.retro68.multiversal}/. $out/powerpc-apple-macos
+              ${pkgs.xorg.lndir}/bin/lndir -silent ${pkgs.retro68.tools}/. $out
+              ${pkgs.xorg.lndir}/bin/lndir -silent ${pkgs.retro68.hfsutils}/. $out
+              ${pkgs.xorg.lndir}/bin/lndir -silent ${pkgs.cmake}/. $out
+              ${pkgs.xorg.lndir}/bin/lndir -silent ${pkgs.gnumake}/. $out
+              ${pkgs.xorg.lndir}/bin/lndir -silent ${pkgs.ninja}/. $out
 
-            ${pkgs.rsync}/bin/rsync -a ${self'.legacyPackages.crossPkgs.m68k.buildPackages.retro68.gcc_unwrapped}/. $out
-            ${pkgs.rsync}/bin/rsync -a ${self'.legacyPackages.crossPkgs.powerpc.buildPackages.retro68.gcc_unwrapped}/. $out
-            ${pkgs.rsync}/bin/rsync -a ${self'.legacyPackages.crossPkgs.carbon.buildPackages.retro68.gcc_unwrapped}/. $out
-          '';
+              ${pkgs.rsync}/bin/rsync -a ${self'.legacyPackages.crossPkgs.m68k.buildPackages.retro68.gcc_unwrapped}/. $out
+              ${pkgs.rsync}/bin/rsync -a ${self'.legacyPackages.crossPkgs.powerpc.buildPackages.retro68.gcc_unwrapped}/. $out
+              ${pkgs.rsync}/bin/rsync -a ${self'.legacyPackages.crossPkgs.carbon.buildPackages.retro68.gcc_unwrapped}/. $out
+            '';
 
-          packages.samples = pkgs.linkFarm "Retro68-Samples" [
-            { name = "m68k"; path = self'.legacyPackages.crossPkgs.m68k.retro68.samples; }
-            { name = "powerpc"; path = self'.legacyPackages.crossPkgs.powerpc.retro68.samples; }
-            { name = "carbon"; path = self'.legacyPackages.crossPkgs.carbon.retro68.samples; }
-          ];
+            samples = pkgs.linkFarm "Retro68-Samples" [
+              { name = "m68k"; path = self'.legacyPackages.crossPkgs.m68k.retro68.samples; }
+              { name = "powerpc"; path = self'.legacyPackages.crossPkgs.powerpc.retro68.samples; }
+              { name = "carbon"; path = self'.legacyPackages.crossPkgs.carbon.retro68.samples; }
+            ];
+          } // lib.mapAttrs'
+            (name: cross: lib.nameValuePair "samples-${name}" cross.retro68.samples)
+            self'.legacyPackages.crossPkgs;
         };
       flake = {
         overlays.default =
