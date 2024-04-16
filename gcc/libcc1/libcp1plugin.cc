d55d90a9e507c6c8e8ae45501c44acefd4a43c24
Fredrik Andersson <fredrikandersson@mac.com>
2024-04-16 20:12:00 +0200

    Fixes include poisoning on sonoma

--- gcc/libcc1/libcp1plugin.cc
+++ gcc/libcc1/libcp1plugin.cc
@@ -33,6 +33,7 @@
 #undef PACKAGE_VERSION
 
 #define INCLUDE_MEMORY
+#define INCLUDE_VECTOR
 #include "gcc-plugin.h"
 #include "system.h"
 #include "coretypes.h"
@@ -71,7 +72,6 @@
 #include "rpc.hh"
 #include "context.hh"
 
-#include <vector>
 
 using namespace cc1_plugin;
 
